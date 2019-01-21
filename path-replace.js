19e83b165b0467f9c18b06d527cfdb565c4134f5
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-01-21 16:26:25 -0200

    Publishing project to Github

---
+++ path-replace.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const replace = require('replace-in-file')
+const tsConfig = require('./tsconfig.json')
+const { compilerOptions: { baseUrl, outDir, paths: TS_PATHS } } = tsConfig
+const SEPARATE_DIR = '/'
+const ROOT_PATH = `${outDir}${baseUrl.replace('.', '')}${SEPARATE_DIR}`
+
+Object.entries(TS_PATHS).forEach(([ key, [ value ] ]) => {
+    let filePath = ''
+    const options = {
+        files: `${ROOT_PATH}**/*.js`,
+        from: file => {
+            filePath = file
+            return new RegExp(`((require)+(\\(")+((${key})))`, 'g')
+        },
+        to: match => {
+            const relativePath = filePath.replace(ROOT_PATH, '')
+            const diffPath = relativePath.substring(0, (relativePath.lastIndexOf(SEPARATE_DIR) + 1))
+            const pathsToBack = diffPath.split(SEPARATE_DIR).filter(dir => dir.length)
+            const absoluteKey = key.substring(0, (key.length - 1))
+            const absolutePath = value.substring(0, (value.length - 1))
+            if (pathsToBack.length) {
+                const backsPath = pathsToBack.map(() => '../').join('')
+                return match.replace(absoluteKey, `${backsPath}${absolutePath}`)
+            }
+            return match.replace(absoluteKey, `./${absolutePath}`)
+        }
+    }
+
+    try {
+        const changes = replace.sync(options)
+        console.log('[Path Replace] Modified files => ', changes.join(', '))
+    } catch (error) {
+        console.error('[Path Replace] Error on trying modified files => ', error)
+    }
+})
